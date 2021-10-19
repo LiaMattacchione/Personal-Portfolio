75ef393d667c0c39da38931eda9cb412150e6160
Lola <lia1m@aol.com>
2021-10-18 23:24:09 -0400

    adjusted image opacity on profile cards

--- photo-port/src/components/Portfolio/index.js
+++ photo-port/src/components/Portfolio/index.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { Card,Row,Col,Button } from 'react-bootstrap';
 
-// import proOne from '../../assets/large/ChefsWay_Icon.png';
-// import proTwo from '../../assets/large/budget.png';
-// import proThree from '../../assets/large/COVID.png';
-// import proFour from '../../assets/large/GEDC-icon.png';
-// import proFive from '../../assets/large/Healthy-icon.png';
-// import proSix from '../../assets/large/OKA-icon.png';
-
 
 function Portfolio(profile) {
     return (
@@ -18,7 +11,7 @@ function Portfolio(profile) {
                     {profile.portfolioItems.map((portfolioItem) => (<Col key={portfolioItem.id}>
                             <Card className="bg-dark text-white">
                                 <div className="text-center">
-                                    <Card.Img variant="top" src={portfolioItem.imgurl} style={{ width: "80%" }} alt="" />
+                                    <Card.Img class="portfolioImage" variant="top" src={portfolioItem.imgurl} style={{ width: "80%" }} alt="" />
                                 </div>
                                 <Card.ImgOverlay>
                                     <Card.Body>
